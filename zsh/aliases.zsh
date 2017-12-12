c362f164d9cf86ff7885982404e687da61e51499
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2017-12-12 10:00:40 +0100

    Update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,6 +4,7 @@ alias dev="cd ~/Develop/"
 alias work="cd ~/Develop/Work/"
 alias ikea="cd ~/Develop/Work/IKEA-M2/"
 alias index_service="cd ~/Develop/Work/IKEA-M2/index-service/"
-alias search_results="cd ~/Develop/Work/IKEA-M2/search-results"
+alias sresults="cd ~/Develop/Work/IKEA-M2/search-results"
+alias sadmin="cd ~/Develop/Work/IKEA-M2/search-admin"
 
 alias nodenv_set="sh ~/Develop/Work/Jayway/nodenv/nodenv.sh"
