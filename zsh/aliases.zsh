9865d3e49c76a6662b3a19b47415a169271a1f81
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-02-17 20:21:49 +0100

    Update sdeploy alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,7 +8,7 @@ alias ikea="cd ~/Develop/Work/IKEA-M2/"
 alias index_service="cd ~/Develop/Work/IKEA-M2/index-service/"
 alias sresults="cd ~/Develop/Work/IKEA-M2/search-results"
 alias sadmin="cd ~/Develop/Work/IKEA-M2/search-admin"
-alias sdeploy="cd ~/Develop/Work/IKEA-M2/search-deploy"
+alias sdeploy="cd ~/Develop/Go/src/github.com/mammutmw/search-deploy"
 
 # Node
 alias nodenv_set="sh ~/Develop/Work/Jayway/nodenv/nodenv.sh"
