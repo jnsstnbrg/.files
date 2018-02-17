4f634ec4b3c12c2886daed5f785d1f9f873aafe3
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-02-17 15:24:10 +0100

    Add sdeploy alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,6 +8,7 @@ alias ikea="cd ~/Develop/Work/IKEA-M2/"
 alias index_service="cd ~/Develop/Work/IKEA-M2/index-service/"
 alias sresults="cd ~/Develop/Work/IKEA-M2/search-results"
 alias sadmin="cd ~/Develop/Work/IKEA-M2/search-admin"
+alias sdeploy="cd ~/Develop/Work/IKEA-M2/search-deploy"
 
 # Node
 alias nodenv_set="sh ~/Develop/Work/Jayway/nodenv/nodenv.sh"
