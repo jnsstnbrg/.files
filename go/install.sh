dc67d9652f08acd78e78050dcb107c0e0cc2eca9
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-03-29 18:14:48 +0200

    Remove vim-go install

--- go/install.sh
+++ go/install.sh
@@ -4,8 +4,3 @@ mkdir -p $HOME/Develop/Go
 mkdir -p $HOME/Develop/Go/src/github.com/user
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-DIRECTORY="~/.vim/plugged/vim-go"
-if [ ! -d "$DIRECTORY" ]; then
-  git clone https://github.com/fatih/vim-go.git $DIRECTORY
-fi
