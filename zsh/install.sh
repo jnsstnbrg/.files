7ba439e5f378eef08b304de2228a9253420b30ed
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-03-29 18:15:14 +0200

    Move oh-my-zsh installation

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Install oh my zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
 DIRECTORY=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 if [[ ! -d "$DIRECTORY" ]]; then
   echo "Cloning zsh-syntax-highlighting"
