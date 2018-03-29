e2e42c956b6c096db186e507e166ccf8e3b270f4
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-03-29 18:18:28 +0200

    Move private aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,16 +1,6 @@
 # Vim
 alias vim="nvim"
 
-# Dir
-alias dev="cd ~/Develop/"
-alias work="cd ~/Develop/Work/"
-alias ikea="cd ~/Develop/Work/IKEA-M2/"
-alias index_service="cd ~/Develop/Work/IKEA-M2/index-service/"
-alias sresults="cd ~/Develop/Work/IKEA-M2/search-results"
-alias sadmin="cd ~/Develop/Work/IKEA-M2/search-admin"
-alias sdeploy="cd ~/Develop/Go/src/search-deploy"
-alias crawler="cd ~/Develop/Go/src/mammutmw/content-crawler"
-
 # Node
 alias nodenv_set="sh ~/Develop/Work/Jayway/nodenv/nodenv.sh"
 
