1807676dfe9813c360ad61ae6505e1878f9b575a
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-03-29 16:35:18 +0200

    Update vim-go install flow

--- go/install.sh
+++ go/install.sh
@@ -3,7 +3,9 @@
 mkdir -p $HOME/Develop/Go
 mkdir -p $HOME/Develop/Go/src/github.com/user
 
-./$GOPATH/src/github.com/nsf/gocode/nvim/symlink.sh
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-git clone https://github.com/fatih/vim-go.git ~/.vim/plugged/vim-go
+
+DIRECTORY="~/.vim/plugged/vim-go"
+if [ ! -d "$DIRECTORY" ]; then
+  git clone https://github.com/fatih/vim-go.git $DIRECTORY
+fi
