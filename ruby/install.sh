1c51dc3115832f4b8a0f356f8807ef4732041fd9
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-03-29 16:37:47 +0200

    Update Ruby install instructions

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-rbenv install 2.5.0
-rbenv global 2.5.0
-rbenv rehash
+rubies=(
+  '2.5.0'
+)
 
-gem install neovim
+# Ruby Installation
+install_ruby () {
+  if hash rbenv 2>/dev/null; then
+    for rb in "${rubies[@]}"; do
+      if [ "$(rbenv versions | grep -Eio $rb)" == $rb ]; then
+        printf "\e[0;32m       * ruby $rb is already installed...\n\e[0m"
+      fi
+
+      if [ "$(rbenv versions | grep -Eio $rb)" == "" ]; then
+        rbenv install $rb
+      fi
+    done
+
+    rbenv global "${rubies}"
+    sleep 1
+    sudo gem install bundler
+  fi
+}
+
+if [ "$(gem list --local | grep -Eio "neovim")" == "" ]; then
+  echo "Installing neovim"
+  sudo gem install neovim
+fi
