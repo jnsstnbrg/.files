a39cc500990041bc9bedfb2c8d137a1b57850dd9
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-04-24 16:18:26 +0200

    Update colors of git log alias

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,3 +1,3 @@
 function glol() {
-  git log --graph --pretty='%h%Creset - %C(cyan)%ad%Creset -%C(cyan)%d%Creset %s %C(cyan)<%an>%Creset' --date=format:'%Y-%m-%d %H:%M:%S' --abbrev-commit -${1:-30} HEAD
+  git log --graph --pretty='%h%Creset - %C(cyan)%ad%Creset -%C(yellow)%d%Creset %s %C(cyan)<%an>%Creset' --date=format:'%Y-%m-%d %H:%M:%S' --abbrev-commit -${1:-30} HEAD
 }
