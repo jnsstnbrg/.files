a1f94559ff6175da9c4a5a6d5b40c1df9db82186
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-05-07 21:15:41 +0200

    Add gloltag alias

--- git/functions.zsh
+++ git/functions.zsh
@@ -1,3 +1,7 @@
 function glol() {
   git log --graph --pretty='%h%Creset - %C(cyan)%ad%Creset -%C(yellow)%d%Creset %s %C(cyan)<%an>%Creset' --date=format:'%Y-%m-%d %H:%M:%S' --abbrev-commit -${1:-30} HEAD
 }
+
+function gloltag() {
+  git log --graph --pretty='%h%Creset - %C(cyan)%ad%Creset -%C(yellow)%d%Creset %s %C(cyan)<%an>%Creset' --date=format:'%Y-%m-%d %H:%M:%S' --abbrev-commit -${1:-30} $(git describe --tags --abbrev=0 @^)..@
+}
