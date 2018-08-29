98079b885d5b8bcdeb3b6d8b480ba08d03f361cb
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2018-08-29 13:23:48 +0200

    Version control tmuxinator projects

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if [[ ! -d "~/.tmux/plugins/tpm" ]]; then
+if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   mkdir -p  ~/.tmux/plugins/tpm
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
+
+for filename in ${DOTFILES}/tmux/projects/*; do
+  if [[ ! -f ~/.config/tmuxinator/${filename##*/} ]]; then
+    ln -s ${filename} ~/.config/tmuxinator
+  fi
+done
