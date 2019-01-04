119f46842f395e684911275621071c8c4878451c
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-01-04 19:43:40 +0100

    Move vim-plug to correct place ¯\_(ツ)_/¯

--- go/install.sh
+++ go/install.sh
@@ -2,5 +2,3 @@
 
 mkdir -p $HOME/Develop/Go
 mkdir -p $HOME/Develop/Go/src/github.com/user
-
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- nvim/install.sh
+++ nvim/install.sh
@@ -10,5 +10,7 @@ let &packpath = &runtimepath
 source ~/.vimrc
 EOF
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 # nvim +PlugInstall +qall
 # nvim +GoInstallBinaries +qall
