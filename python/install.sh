2cb0858c6216c9bd7a3a0f6ed285319b19d8ce0e
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-04-19 16:41:26 +0200

    Update python version

--- python/install.sh
+++ python/install.sh
@@ -6,7 +6,7 @@ install_pyenv() {
     pyenv install 2.7.11
     pyenv virtualenv 2.7.11 neovim2
   fi
-  if [ "$(pyenv versions | grep -Eio "3.4.4")" == "" ]; then
+  if [ "$(pyenv versions | grep -Eio "3.7.0")" == "" ]; then
     echo "Installing 3.7.0"
     pyenv install 3.7.0
     pyenv virtualenv 3.7.0 neovim3
