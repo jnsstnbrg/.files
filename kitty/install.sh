67bb9b8774dde2b68ba216a99c096de6d7ed335e
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-05-15 11:21:20 +0200

    Add macos_custom_beam_cursor setting

--- kitty/install.sh
+++ kitty/install.sh
@@ -1,7 +1,9 @@
 if [ ! -f ~/.config/kitty/snazzy.conf ]; then
+  mkdir -p "~/.config/kitty"
   ln -s "${DOTFILES}/kitty/snazzy.conf" ~/.config/kitty/snazzy.conf
 fi
 
 if [ ! -f ~/.config/kitty/kitty.conf ]; then
+  mkdir -p "~/.config/kitty"
   ln -s "${DOTFILES}/kitty/kitty.conf" ~/.config/kitty/kitty.conf
 fi
