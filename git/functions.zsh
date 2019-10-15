abd2f2c970dca6392f1392f96ceb3b2ec5f580c4
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2019-10-15 20:58:58 +0200

    Add gcount git alias

--- git/functions.zsh
+++ git/functions.zsh
@@ -5,3 +5,7 @@ function glol() {
 function gloltag() {
   git log --graph --pretty='%h%Creset - %C(cyan)%ad%Creset -%C(yellow)%d%Creset %s %C(cyan)<%an>%Creset' --date=format:'%Y-%m-%d %H:%M:%S' --abbrev-commit -${1:-30} $(git describe --tags --abbrev=0 @^)..@
 }
+
+function gcount() {
+  git diff --numstat --pretty="%H" $1 | awk 'NF==3 {plus+=$1; minus+=$2} END {printf("+%d, -%d\n", plus, minus)}'
+}
