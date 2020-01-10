89aa095052da8e56b2026a397d0e7ca7e036bf1c
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-01-10 12:53:40 +0100

    Remove tmuxinator

--- tmux/install.sh
+++ tmux/install.sh
@@ -5,11 +5,6 @@ if [[ ! -d ~/.tmux/plugins/tpm ]]; then
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
-if [ "$(gem list --local | grep -Eio "tmuxinator")" == "" ]; then
-  eval "$(rbenv init -)"
-  gem install --user tmuxinator
-fi
-
 for filename in ${DOTFILES}/tmux/projects/*; do
   mkdir -p ~/.config/tmuxinator
   if [[ ! -f ~/.config/tmuxinator/${filename##*/} ]]; then
