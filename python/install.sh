9695e1e8bfe54782e1051a3d5a96367fef052dd9
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-01-14 10:04:28 +0100

    Update python versions

--- python/install.sh
+++ python/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 install_pyenv() {
-  if [ "$(pyenv versions | grep -Eio "2.7.11")" == "" ]; then
-    echo "Installing 2.7.11"
-    pyenv install 2.7.11
-    pyenv virtualenv 2.7.11 neovim2
+  if [ "$(pyenv versions | grep -Eio "2.7.16")" == "" ]; then
+    echo "Installing 2.7.16"
+    pyenv install 2.7.16
+    pyenv virtualenv 2.7.16 neovim2
   fi
-  if [ "$(pyenv versions | grep -Eio "3.7.0")" == "" ]; then
-    echo "Installing 3.7.0"
-    pyenv install 3.7.0
-    pyenv virtualenv 3.7.0 neovim3
+  if [ "$(pyenv versions | grep -Eio "3.8.0")" == "" ]; then
+    echo "Installing 3.8.0"
+    pyenv install 3.8.0
+    pyenv virtualenv 3.8.0 neovim3
   fi
 
   pyenv activate neovim2
