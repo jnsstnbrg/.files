e6b5e92fd51bcdfbee1ae52d94ef274a26382540
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-01-14 10:03:38 +0100

    Fix installation echo's

--- neovim/install.sh
+++ neovim/install.sh
@@ -15,13 +15,17 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.c
 # nvim +PlugInstall +qall
 # nvim +GoInstallBinaries +qall
 
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
 install_plugins() {
-  bullet "Running PlugInstall... "
+  echo "Running PlugInstall... "
   nvim +PlugInstall +UpdateRemotePlugins +qall
   success "done"
 
   extensions="coc-tsserver coc-eslint coc-json coc-html coc-css coc-tag coc-vetur coc-gocode coc-pairs coc-prettier"
-  bullet "Installing Coc extensions... "
+  echo "Installing Coc extensions... "
   mkdir -p ~/.config/coc/extensions
   cd ~/.config/coc/extensions
   npm install -S $extensions
