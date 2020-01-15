f63e26fba4d977dddc33a6d6400b306e40f70123
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-01-15 16:28:11 +0100

    Remove pyenv prompt info

--- python/init.zsh
+++ python/init.zsh
@@ -3,12 +3,4 @@ if (( $+commands[pyenv] )); then
   if (( $+commands[pyenv-virtualenv-init] )); then
     eval "$(pyenv virtualenv-init -)"
   fi
-  function pyenv_prompt_info() {
-    echo "$(pyenv version-name)"
-  }
-else
-  # fallback to system python
-  function pyenv_prompt_info() {
-    echo "system: $(python -V 2>&1 | cut -f 2 -d ' ')"
-  }
 fi
