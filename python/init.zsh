15cce501eb081554f1924c6b53a0b60be2d09c1d
Jonas Stenberg <jonas.d.stenberg@gmail.com>
2020-09-23 15:50:40 +0200

    Remove python

--- python/init.zsh
+++ python/init.zsh
@@ -1,6 +1,14 @@
-if (( $+commands[pyenv] )); then
-  eval "$(pyenv init -)"
-  if (( $+commands[pyenv-virtualenv-init] )); then
-    eval "$(pyenv virtualenv-init -)"
-  fi
-fi
+# if (( $+commands[pyenv] )); then
+#   eval "$(pyenv init -)"
+#   if (( $+commands[pyenv-virtualenv-init] )); then
+#     eval "$(pyenv virtualenv-init -)"
+#   fi
+# fi
+#
+# if [[ -n $VIRTUAL_ENV && -e "${VIRTUAL_ENV}/bin/activate" ]]; then
+#   source "${VIRTUAL_ENV}/bin/activate"
+# fi
+#
+# if (( $+commands[pipenv] )); then
+#   eval "$(pipenv --completion)"
+# fi
